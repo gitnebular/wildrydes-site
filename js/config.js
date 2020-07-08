6ac49bc9cdac5dbb1d494a57f6d88630e09077fe
Jesse Wright <jcwconsultinginc@gmail.com>
2020-07-07 19:18:27 -0700

    added region

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-west-2_sjEblgs1Y', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '748hb1e5g9vkqd0ub873u0gavd', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
